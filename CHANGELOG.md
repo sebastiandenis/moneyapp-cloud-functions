03ac354f9bcd2a9a327612548d1dd5b89d3db3b9
Sebastian Denis <sebastian.denis@outlook.com>
2017-06-23 16:42:20 +0200

    0.5.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+0.5.0
+  * Add remove saving line function
+
 0.4.0
   * Add saving item function
--- functions/index.js
+++ functions/index.js
@@ -1,24 +1,71 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
-    console.log("Hello form console of Firebase!");
-});
+//version: 0.5.0
+
+
+/******************************************************************
+
+                    SAVINGS
+
+ *******************************************************************/
+
+exports.removeSavingLine = functions.database
+    .ref('/savings/{pushId}/lines/{pushId2}')
+    .onWrite(event => {
+        /*
+    Funkcja wywoaływana podczas usuwania linii z savings
+    1) usuń całą linię z savingLines
+    2) usuń wszystkie savingItems dla savingLineId
+*/
+
+        if (event.data.exists()) {
+            return; //wyjdź jeżeli dotyczy zapisu
+        }
+        const savingId = event.params.pushId;
+        const savingLineId = event.params.pushId2;
+
+        //console.log("Saving Id: ", savingId);
+        //console.log("Saving Line Id: ", savingLineId);
+
+        const root = event.data.ref.root;
+        const slRef = root.child(`/savingLines/${savingLineId}`);
+        let savingItemIds = [];
+        return slRef.once('value')
+            .then(snap => {
+                const savingLine = snap.val();
+                if (savingLine && savingLine.hasOwnProperty('savingItems')) {
+                    Object.keys(savingLine.savingItems).forEach(function (key) {
+                        return root.child(`/savingItems/${key}`).remove();
+                    });
+                }
+
+                return slRef.remove();
+            })
+            .catch(error => {
+                console.log(error);
+            })
+
+    });
+
+
+
+/******************************************************************
+
+                    BUDGET
+
+ *******************************************************************/
+
 
 exports.addOutgo = functions.database
-    /*
-        Funkcja wywoaływana podczas dodania nowej pozycji w bazie outgoes
-        1) dodaj outgo ID do odpowiedniej linii
-        2) zmniejsz w linii cashLeft
-        3) zwiększ w linii noOutgoes
-        4) w budget zmniejsz cashLeft
-    */
     .ref('/outgoes/{pushId}')
     .onWrite(event => {
-      //  console.log("Starting function version 0.3.0...");
+        /*
+    Funkcja wywoaływana podczas dodania nowej pozycji w bazie outgoes
+    1) dodaj outgo ID do odpowiedniej linii
+    2) zmniejsz w linii cashLeft
+    3) zwiększ w linii noOutgoes
+    4) w budget zmniejsz cashLeft
+*/
         let budgetId = null;
         const outgoId = event.params.pushId;
         const amount = event.data.val().amount;
@@ -37,13 +84,13 @@ exports.addOutgo = functions.database
                 budgetId = budgetLine.budgetId;
                 if (!budgetLine.hasOwnProperty('outgoes')) {
                     //jeżeli nie ma żadnego outgo to utwórz obiekt
-               //     console.log("Nie ma property outgoes!");
+                    //     console.log("Nie ma property outgoes!");
                     Object.assign(budgetLine, { outges: { [outgoId]: true } });
                 } else {
                     //w przeciwnym wypadku dodaj tylko referencję
                     Object.assign(budgetLine.outgoes, { [outgoId]: true });
                 }
-             //   console.log("budgetLine from Cloud Functions: ", budgetLine);
+                //   console.log("budgetLine from Cloud Functions: ", budgetLine);
                 return blRef.set(budgetLine)
             }).then(() => {
                 return root.child(`/budgets/${budgetId}`).once('value')
@@ -57,18 +104,19 @@ exports.addOutgo = functions.database
                 console.log(error);
             })
 
-    })
+    });
 
-    exports.addSavingItem = functions.database
-    /*
-        Funkcja wywoaływana podczas dodania nowej pozycji w bazie savingItems
-        1) dodaj saving ID do odpowiedniej linii
-        2) zwiększ w linii cashLeft
-        4) w savings zmniejsz cashLeft (total)
-    */
+exports.addSavingItem = functions.database
     .ref('/savingItems/{pushId}')
     .onWrite(event => {
-      //  console.log("Starting function version 0.4.0...");
+        /*
+    Funkcja wywoaływana podczas dodania nowej pozycji w bazie savingItems
+    1) dodaj saving ID do odpowiedniej linii
+    2) zwiększ w linii cashLeft
+    4) w savings zmniejsz cashLeft (total)
+    5) w odpowiedniej linii dodaj savingItem
+*/
+
         let savingId = null;
         const savingItemId = event.params.pushId;
         const amount = event.data.val().amount;
@@ -82,13 +130,13 @@ exports.addOutgo = functions.database
                 savingId = savingLine.savingId;
                 if (!savingLine.hasOwnProperty('savingItems')) {
                     //jeżeli nie ma żadnego outgo to utwórz obiekt
-                  //  console.log("Nie ma property savingItems!");
+                    //  console.log("Nie ma property savingItems!");
                     Object.assign(savingLine, { savingItems: { [savingItemId]: true } });
                 } else {
                     //w przeciwnym wypadku dodaj tylko referencję
                     Object.assign(savingLine.savingItems, { [savingItemId]: true });
                 }
-             //   console.log("savingLine from Cloud Functions: ", savingLine);
+                //   console.log("savingLine from Cloud Functions: ", savingLine);
                 return slRef.set(savingLine)
             }).then(() => {
                 return root.child(`/savings/${savingId}`).once('value')
@@ -102,4 +150,4 @@ exports.addOutgo = functions.database
                 console.log(error);
             })
 
-    })
+    });
